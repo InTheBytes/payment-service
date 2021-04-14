859e42de2c53a475fe3c01e42948bd9b9a22ccc9
lexinelson <lexi.nelson@smoothstack.com>
2021-04-14 15:11:29 -0700

    Project Initialization Commit

---
+++ src/test/java/com/inthebytes/searchservice/SearchserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inthebytes.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
