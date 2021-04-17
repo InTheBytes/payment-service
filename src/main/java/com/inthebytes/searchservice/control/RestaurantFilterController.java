e0ec6d43aa968b0f0cc5d99d252913472819f391
lexinelson <lexi.nelson@smoothstack.com>
2021-04-17 15:36:57 -0700

    ITB-15: Included POST for search results + tests for POST

--- src/main/java/com/inthebytes/searchservice/control/RestaurantFilterController.java
+++ src/main/java/com/inthebytes/searchservice/control/RestaurantFilterController.java
@@ -3,10 +3,13 @@ package com.inthebytes.searchservice.control;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +18,13 @@ import com.inthebytes.searchservice.dto.RestaurantDTO;
 import com.inthebytes.searchservice.service.RestaurantFilterService;
 
 @RestController
-@RequestMapping("/restaurants")
+@RequestMapping("/restaurants/filter")
 public class RestaurantFilterController {
 	
 	@Autowired
 	RestaurantFilterService service;
 	
-	@GetMapping(value="/filter")
+	@GetMapping(value="")
 	public ResponseEntity<List<RestaurantDTO>> filter(
 			@RequestParam(value = "zip-code", required = false, defaultValue = "0") Integer zip,
 			@RequestParam(value = "city", required = false, defaultValue = "_") String city,
@@ -34,6 +37,40 @@ public class RestaurantFilterController {
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 		else
 			return ResponseEntity.ok().body(results);
+	}
+	
+	@PostMapping(value="")
+	public ResponseEntity<List<RestaurantDTO>> filterResults(@Valid @RequestBody List<RestaurantDTO> results,
+			@RequestParam(value = "zip-code", required = false, defaultValue = "0") Integer zip,
+			@RequestParam(value = "city", required = false, defaultValue = "_") String city,
+			@RequestParam(value = "cuisine", required = false, defaultValue = "_") String cuisine,
+			@RequestParam(value = "price-max", required = false, defaultValue = "0.0") Double priceMax,
+			@RequestParam(value = "price-min", required = false, defaultValue = "0.0") Double priceMin) {
+		
+		List<RestaurantDTO> filtered = layerFilters(results, zip, city, cuisine, priceMax, priceMin);
+		if (filtered == null || filtered.size() == 0)
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		else
+			return ResponseEntity.ok().body(filtered);
+	}
+	
+	private List<RestaurantDTO> layerFilters(List<RestaurantDTO> results, 
+			Integer zip, String city, String cuisine, Double priceMax, Double priceMin) {
+		if (zip != 0)
+			results = service.filterByZip(results, zip);
+		if (!"_".equals(city))
+			results = service.filterByCity(results, city);
+		if (!"_".equals(cuisine))
+			results = service.filterByCuisine(results, cuisine);
+		if (priceMax != 0.0 && priceMin != 0.0)
+			results = service.inPriceBracket(results, priceMin, priceMax);
+		else {
+			if (priceMax != 0.0)
+				results = service.lessThanPrice(results, priceMax);
+			if (priceMin != 0.0)
+				results = service.greaterThanPrice(results, priceMin); 
+		}
+		return results;
 	}
 	
 	private List<RestaurantDTO> applyFilters(Integer zip, String city, String cuisine, Double priceMax, Double priceMin) {
--- src/test/java/com/inthebytes/searchservice/control/RestaurantFilterControllerTest.java
+++ src/test/java/com/inthebytes/searchservice/control/RestaurantFilterControllerTest.java
@@ -39,19 +39,19 @@ public class RestaurantFilterControllerTest {
 
 	@MockBean
 	RestaurantFilterService service;
-	
+
 	@Autowired
 	MockMvc mock;
-	
+
 	private final List<RestaurantDTO> results = new ArrayList<RestaurantDTO>();
 	private final List<RestaurantDTO> smallResults = new ArrayList<RestaurantDTO>();
-	
+
 	@BeforeAll
 	public void setUp() {
 		LocationDTO location = new LocationDTO("123", "Main", "Sacramento", "CA", 11111);
 		List<FoodDTO> foods = new ArrayList<FoodDTO>();
 		foods.add(new FoodDTO("Food", 1.00, "It's food"));
-		
+
 		RestaurantDTO rest1 = new RestaurantDTO("My Burgers", "Fast Food", location);
 		rest1.setFoods(foods);
 		RestaurantDTO rest2 = new RestaurantDTO("My Bistro", "French", location);
@@ -60,143 +60,294 @@ public class RestaurantFilterControllerTest {
 		rest3.setFoods(foods);
 		RestaurantDTO rest4 = new RestaurantDTO("My Diner", "Pizza", location);
 		rest4.setFoods(foods);
-		
+
 		results.add(rest1);
 		results.add(rest2);
 		results.add(rest3);
 		results.add(rest4);
 		System.out.println("Stuff added: " + results.toString());
-	
+
 		smallResults.add(rest1);
 		smallResults.add(rest2);
 	}
-	
+
 	@Test
 	public void filterByZipTest() throws Exception {
 		when(service.filterByZip(11111)).thenReturn(results);
-		
+
 		mock.perform(get("/restaurants/filter?zip-code=11111")
 				.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(MockMvcResultMatchers.status().isOk())
 		.andExpect(jsonPath("$", hasSize(4)));
 	}
-	
+
 	@Test
 	public void filterByZipEmptyTest() throws Exception {
 		when(service.filterByZip(11111)).thenReturn(null);
-		
+
 		mock.perform(get("/restaurants/filter?zip-code=11111"))
 		.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
-	
+
+	@Test
+	public void filterResultByZipTest() throws Exception {
+		when(service.filterByZip(results, 11111)).thenReturn(smallResults);
+
+		mock.perform(post("/restaurants/filter?zip-code=11111")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(jsonPath("$", hasSize(2)));
+	}
+
+	@Test
+	public void filterResultsByZipEmptyTest() throws Exception {
+		when(service.filterByZip(results, 11111)).thenReturn(null);
+
+		mock.perform(post("/restaurants/filter?zip-code=11111")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
+
 	@Test
 	public void filterByCityTest() throws Exception {
 		when(service.filterByCity("Sacramento")).thenReturn(results);
-		
+
 		mock.perform(get("/restaurants/filter?city=Sacramento")
 				.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(MockMvcResultMatchers.status().isOk())
 		.andExpect(jsonPath("$", hasSize(4)));
 	}
-	
+
 	@Test
 	public void filterByCityEmptyTest() throws Exception {
 		when(service.filterByCity("Sacramento")).thenReturn(null);
-		
+
 		mock.perform(get("/restaurants/filter?city=Sacramento"))
 		.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
 	
 	@Test
+	public void filterResultsByCityTest() throws Exception {
+		when(service.filterByCity(results, "Sacramento")).thenReturn(smallResults);
+
+		mock.perform(post("/restaurants/filter?city=Sacramento")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(jsonPath("$", hasSize(2)));
+	}
+
+	@Test
+	public void filterResultsByCityEmptyTest() throws Exception {
+		when(service.filterByCity("Sacramento")).thenReturn(null);
+
+		mock.perform(post("/restaurants/filter?city=Sacramento")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
+
+	@Test
 	public void filterByCuisineTest() throws Exception {
 		when(service.filterByCuisine("Any")).thenReturn(results);
-		
+
 		mock.perform(get("/restaurants/filter?cuisine=Any")
 				.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(MockMvcResultMatchers.status().isOk())
 		.andExpect(jsonPath("$", hasSize(4)));
 	}
-	
+
 	@Test
 	public void filterByCuisineEmptyTest() throws Exception {
 		when(service.filterByCuisine("None")).thenReturn(null);
-		
+
 		mock.perform(get("/restaurants/filter?cuisine=None"))
 		.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
 	
+	
+	@Test
+	public void filterResultsByCuisineTest() throws Exception {
+		when(service.filterByCuisine(results, "Any")).thenReturn(smallResults);
+
+		mock.perform(post("/restaurants/filter?cuisine=Any")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(jsonPath("$", hasSize(2)));
+	}
+
+	@Test
+	public void filterResultsByCuisineEmptyTest() throws Exception {
+		when(service.filterByCuisine(results, "None")).thenReturn(null);
+
+		mock.perform(post("/restaurants/filter?cuisine=None")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
+
 	@Test
 	public void filterByMaxPriceTest() throws Exception {
 		when(service.lessThanPrice(10.00)).thenReturn(results);
-		
+
 		mock.perform(get("/restaurants/filter?price-max=10.00")
 				.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(MockMvcResultMatchers.status().isOk())
 		.andExpect(jsonPath("$", hasSize(4)));
 	}
-	
+
 	@Test
 	public void filterByMaxPriceEmptyTest() throws Exception {
 		when(service.lessThanPrice(1.0)).thenReturn(null);
-		
+
 		mock.perform(get("/restaurants/filter?price-max=1.0"))
 		.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
 	
 	@Test
+	public void filterResultByMaxPriceTest() throws Exception {
+		when(service.lessThanPrice(results, 10.00)).thenReturn(smallResults);
+
+		mock.perform(post("/restaurants/filter?price-max=10.00")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(jsonPath("$", hasSize(2)));
+	}
+
+	@Test
+	public void filterResultsByMaxPriceEmptyTest() throws Exception {
+		when(service.lessThanPrice(results, 1.0)).thenReturn(null);
+
+		mock.perform(post("/restaurants/filter?price-max=1.0")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
+
+	@Test
 	public void filterByMinPriceTest() throws Exception {
 		when(service.greaterThanPrice(10.00)).thenReturn(results);
-		
+
 		mock.perform(get("/restaurants/filter?price-min=10.00")
 				.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(MockMvcResultMatchers.status().isOk())
 		.andExpect(jsonPath("$", hasSize(4)));
 	}
-	
+
 	@Test
 	public void filterByMinPriceEmptyTest() throws Exception {
 		when(service.greaterThanPrice(1.0)).thenReturn(null);
-		
+
 		mock.perform(get("/restaurants/filter?price-min=1.0"))
 		.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
 	
 	@Test
+	public void filterResultByMinPriceTest() throws Exception {
+		when(service.greaterThanPrice(results, 10.00)).thenReturn(smallResults);
+
+		mock.perform(post("/restaurants/filter?price-min=10.00")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(jsonPath("$", hasSize(2)));
+	}
+
+	@Test
+	public void filterResultByMinPriceEmptyTest() throws Exception {
+		when(service.greaterThanPrice(results, 1.0)).thenReturn(null);
+
+		mock.perform(post("/restaurants/filter?price-min=1.0")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
+
+	@Test
 	public void filterByPriceRangeTest() throws Exception {
 		when(service.inPriceBracket(10.00, 20.00)).thenReturn(results);
-		
+
 		mock.perform(get("/restaurants/filter?price-min=10&price-max=20")
 				.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(MockMvcResultMatchers.status().isOk())
 		.andExpect(jsonPath("$", hasSize(4)));
 	}
-	
+
 	@Test
 	public void filterByRangeEmptyTest() throws Exception {
 		when(service.inPriceBracket(1.0, 10.0)).thenReturn(null);
-		
+
 		mock.perform(get("/restaurants/filter?price-max=10&price-min=1"))
 		.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
 	
 	@Test
+	public void filterResultByPriceRangeTest() throws Exception {
+		when(service.inPriceBracket(results, 10.00, 20.00)).thenReturn(smallResults);
+
+		mock.perform(post("/restaurants/filter?price-min=10&price-max=20")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(jsonPath("$", hasSize(2)));
+	}
+
+	@Test
+	public void filterResultByRangeEmptyTest() throws Exception {
+		when(service.inPriceBracket(results, 1.0, 10.0)).thenReturn(null);
+
+		mock.perform(post("/restaurants/filter?price-max=10&price-min=1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
+
+	@Test
 	public void filterComboTest() throws Exception {
 		when(service.filterByZip(11111)).thenReturn(results);
 		when(service.filterByCuisine(results, "Any")).thenReturn(smallResults);
-		
+
 		mock.perform(get("/restaurants/filter?zip-code=11111&cuisine=Any")
 				.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(MockMvcResultMatchers.status().isOk())
 		.andExpect(jsonPath("$", hasSize(2)));
-		
+
 	}
-	
+
 	@Test
 	public void filterComboEmptyTest() throws Exception {
 		when(service.filterByZip(11111)).thenReturn(results);
 		when(service.filterByCuisine(results, "Any")).thenReturn(null);
-		
+
 		mock.perform(get("/restaurants/filter?zip-code=11111&cuisine=Any"))
+		.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
+	
+	@Test
+	public void filterComboPostTest() throws Exception {
+		when(service.filterByZip(results, 11111)).thenReturn(results);
+		when(service.filterByCuisine(results, "Any")).thenReturn(smallResults);
+
+		mock.perform(post("/restaurants/filter?zip-code=11111&cuisine=Any")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(jsonPath("$", hasSize(2)));
+
+	}
+
+	@Test
+	public void filterComboEmptyPostTest() throws Exception {
+		when(service.filterByZip(results, 11111)).thenReturn(smallResults);
+		when(service.filterByCuisine(smallResults, "Any")).thenReturn(null);
+
+		mock.perform(post("/restaurants/filter?zip-code=11111&cuisine=Any")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(results)))
 		.andExpect(MockMvcResultMatchers.status().isNoContent());
 	}
 }
