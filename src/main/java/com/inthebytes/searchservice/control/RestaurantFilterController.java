52e911e7652afc7d8a6d2c28248939f0a49db3c4
lexinelson <lexi.nelson@smoothstack.com>
2021-04-17 11:24:58 -0700

    ITB-15: Controller Logic & Test Skeleton

--- src/main/java/com/inthebytes/searchservice/control/RestaurantFilterController.java
+++ src/main/java/com/inthebytes/searchservice/control/RestaurantFilterController.java
@@ -2,6 +2,9 @@ package com.inthebytes.searchservice.control;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +12,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inthebytes.searchservice.dto.RestaurantDTO;
+import com.inthebytes.searchservice.service.RestaurantFilterService;
 
 @RestController
 @RequestMapping("/restaurants/filter")
 public class RestaurantFilterController {
 	
-	@GetMapping(value="", params = {"zip-code", "city", "cuisine", "price-max", "price-min"})
+	@Autowired
+	RestaurantFilterService service;
+	
+	@GetMapping(value="", params = {"zip-code", "city", "cuisine", "price-max", "price-min"}
+//			, produces = MediaType.APPLICATION_JSON_VALUE
+			)
 	public ResponseEntity<List<RestaurantDTO>> filterByZip(
 			@RequestParam(value = "zip-code", required = false) Integer zip,
 			@RequestParam(value = "city", required = false) String city,
@@ -22,6 +31,35 @@ public class RestaurantFilterController {
 			@RequestParam(value = "price-max", required = false) Double priceMax,
 			@RequestParam(value = "price-min", required = false) Double priceMin) {
 		
-		return null;
+		List<RestaurantDTO> results = applyFilters(zip, city, cuisine, priceMax, priceMin);
+		if (results == null) 
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		else
+			return ResponseEntity.ok().body(results);
+	}
+	
+	private List<RestaurantDTO> applyFilters(Integer zip, String city, String cuisine, Double priceMax, Double priceMin) {
+		List<RestaurantDTO> results = null;
+		if (zip != null)
+			results = service.filterByZip(zip);
+		if (city != null)
+			results = (results != null) ? 
+					service.filterByCity(city) : service.filterByCity(results, city);
+		if (cuisine != null)
+			results = (results != null) ? 
+					service.filterByCuisine(cuisine) : service.filterByCuisine(results, cuisine);
+		if (priceMax != null && priceMin != null)
+			results = (results != null) ? 
+					service.inPriceBracket(priceMin, priceMax) : service.inPriceBracket(results, priceMin, priceMax);
+		else {
+			if (priceMax != null)
+				results = (results != null) ?
+						service.lessThanPrice(priceMax) : service.lessThanPrice(results, priceMax);
+			if (priceMin != null)
+				results = (results != null) ?
+						service.greaterThanPrice(priceMin) : service.greaterThanPrice(results, priceMin); 
+		}
+		return results;
+			
 	}
 }
---
+++ src/test/java/com/inthebytes/searchservice/control/RestaurantFilterControllerTest.java
@@ -0,0 +1,61 @@
+package com.inthebytes.searchservice.control;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.inthebytes.searchservice.dto.RestaurantDTO;
+import com.inthebytes.searchservice.service.RestaurantFilterService;
+
+@WebMvcTest(RestaurantFilterController.class)
+@AutoConfigureMockMvc
+public class RestaurantFilterControllerTest {
+
+	@MockBean
+	RestaurantFilterService service;
+	
+	@Autowired
+	MockMvc mock;
+	
+	@Test
+	public void filterByZipTest() {
+		
+	}
+	
+	@Test
+	public void filterByCityTest() {
+		
+	}
+	
+	@Test
+	public void filterByCuisineTest() {
+		
+	}
+	
+	@Test
+	public void filterByMaxPriceTest() {
+		
+	}
+	
+	@Test
+	public void filterByMinPriceTest() {
+		
+	}
+	
+	@Test
+	public void filterByPriceRangeTest() {
+		
+	}
+	
+	@Test
+	public void filterComboTest() {
+		
+	}
+}
