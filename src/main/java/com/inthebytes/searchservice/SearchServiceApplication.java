143ee426c43a80c93992dcd379b9110e9f249248
lexinelson <lexi.nelson@smoothstack.com>
2021-05-09 13:23:49 -0700

    Renamed main application for conflict

--- src/main/java/com/inthebytes/searchservice/SearchServiceApplication.java
+++ src/main/java/com/inthebytes/searchservice/SearchServiceApplication.java
@@ -1,11 +1,12 @@
-  package com.inthebytes.searchservice;
-
+package com.inthebytes.searchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SearchServiceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SearchServiceApplication.class, args);
 	}
