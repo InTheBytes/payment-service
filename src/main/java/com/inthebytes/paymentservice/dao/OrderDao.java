4fac808e45865a7a458cc7c5e14808711d1037b8
Lexi Nelson <lexi.nelson@smoothstack.com>
2021-07-28 13:04:12 -0700

    ITB-21: Removed unused user entity

--- src/main/java/com/inthebytes/paymentservice/dao/OrderDao.java
+++ src/main/java/com/inthebytes/paymentservice/dao/OrderDao.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.inthebytes.paymentservice.entity.Order;
-import com.inthebytes.paymentservice.entity.User;
 
 @Repository
 public interface OrderDao extends JpaRepository<Order, String> {
